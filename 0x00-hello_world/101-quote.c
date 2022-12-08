38cb4625ff7a8c2b9c248c0fc431ad53d65a53b9
Tosin Iyiola <tosiniyiola91@gmail.com>
2022-12-08 15:30:47 -0800

    advance task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - Print a line of code that return 1
+ *
+ * Return: Always 1.
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
