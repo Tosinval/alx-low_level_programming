c2d92dc5bad0435fc9fb5fa5db5fc434fd377ad4
Tosin Iyiola <tosiniyiola91@gmail.com>
2022-12-08 11:34:37 -0800

    put edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+
 /**
  * main - writing a line of code
  *
- * Return : 0
-*/
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
