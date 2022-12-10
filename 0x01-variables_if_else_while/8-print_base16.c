e8b581b6d88093a17bb39467d2af0cebf713d71b
Tosin Iyiola <tosiniyiola91@gmail.com>
2022-12-10 06:13:18 -0800

    task 8 and 9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print a line of word
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int number;
+	char letter;
+
+	for (number = 0; number < 10; number++)
+		putchar((number % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+	return (0);
+}
