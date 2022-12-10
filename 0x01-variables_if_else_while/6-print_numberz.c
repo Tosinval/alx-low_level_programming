e140a7638efe5416402ce1294994cbf94ac85322
Tosin Iyiola <tosiniyiola91@gmail.com>
2022-12-10 05:36:41 -0800

    print numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - printsingle digit
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		putchar((number % 10) + '0');
+
+	putchar('\n');
+	return (0);
+}
