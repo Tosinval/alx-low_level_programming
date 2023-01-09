6366168925bc4f89ba1fb6bf977c09ee9707e0e6
Tosin Iyiola <tosiniyiola91@gmail.com>
2023-01-09 11:37:22 -0800

    print the program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print the program name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("The name of this program is %s\n", argv[0]);
+
+	return (0);
+}
